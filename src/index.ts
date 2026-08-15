9d7011d2e8b184d614d6e3b8dceee60e2972e90f
Emmanuel Oludare <emmanueldavidson524@gmail.com>
2026-08-15 22:06:04 +0100

    Implemented matches POST and GET routes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from "express";
+import { matchRouter } from "./routes/matches.js";
 
 const app: Application = express();
 const port = 8000;
@@ -7,6 +8,12 @@ app.use(express.json());
 app.get("/", (request: Request, response: Response): void => {
   response.send("LiveScores server is running.");
 });
+
+app.get("/api/v1", (request: Request, response: Response): void => {
+  response.send("Welcome to Live scores API");
+});
+
+app.use('/api/v1/matches', matchRouter);
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
